50a4523f1b4f49997131c5267d1ead116fe20c41
Eronary <tact_decoder_0d@icloud.com>
2025-05-20 16:58:52 +0300

    Add HGET command

---
+++ src/commands/hash/hget.ts
@@ -0,0 +1,21 @@
+import type { Command } from '../../types.js';
+
+/**
+ * Returns the value associated with field in the hash stored at key.
+ * - Available since: 2.0.0.
+ * - Time complexity: O(1)
+ * @param key -
+ * @param field -
+ * @returns The value associated with field in the hash stored at key or null.
+ * @see {@link https://redis.io/commands/hget}
+ */
+export function input(key: string, field: string): Command<string | null> {
+	return {
+		kind: '#schema',
+		args: [
+			'HGET',
+			key,
+			field,
+		],
+	};
+}
